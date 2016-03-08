75ce3e57b0d05c697b7b89d0296d139f6978183a
Richard Caceres <richard@archive.org>
2016-03-07 22:33:07 -0800

    More progress on app

--- bookmarks.js
+++ bookmarks.js
@@ -1,5 +1,23 @@
 Bookmarks = new Mongo.Collection("Bookmarks");
 
+/**
+ * @see http://stackoverflow.com/a/3890175
+ */
+function linkify(inputText) {
+  var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+  //URLs starting with http://, https://, or ftp://
+  replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+  replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+  //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+  replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+  replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+  return replacedText;
+}
+
+
 if (Meteor.isClient) {
 
   Template.body.onCreated(function() {
@@ -11,7 +29,7 @@ if (Meteor.isClient) {
       // var currFilter = this.filter.get();
       var currFilter = Session.get('filter2');
       if (currFilter !== '') {
-        var rg = new RegExp(".*" + currFilter + ".*");
+        var rg = new RegExp(".*" + currFilter + ".*", "i");
         return Bookmarks.find({text: rg}, {sort: {createdAt: -1}});
       } else {
         return Bookmarks.find({}, {sort: {createdAt: -1}});
@@ -31,15 +49,6 @@ if (Meteor.isClient) {
     },
     "keyup .filter": function(event) {
       Session.set('filter2', event.target.value);
-      console.log(event.target.value);
-    },
-    "click .delete": function() {
-      if (confirm("Delete?")) {
-        Bookmarks.remove(this._id);
-      }
-    },
-    "click .edit": function(event) {
-      this.is_editing = 'yes';
     },
   });
 
@@ -49,16 +58,41 @@ if (Meteor.isClient) {
   Template.editable.onCreated(function() {
     this.isEditing = new ReactiveVar(false);
   });
+
   Template.editable.events({
-    'dblclick': function(e, t) {
-      t.isEditing.set( ! t.isEditing.get());
-      console.log(t.isEditing.get() );
+    'dblclick': function(event, context) {
+      context.isEditing.set( ! context.isEditing.get());
+    },
+    'change textarea': function(event, context) {
+      Bookmarks.update(context.data._id, {$set: {text: event.target.value}});
+    },
+    "click .edit": function(event, context) {
+      context.isEditing.set( ! context.isEditing.get());
+    },
+    "click .delete": function(event, context) {
+      if (confirm("Delete?")) {
+        Bookmarks.remove(context.data._id);
+      }
     },
   });
+
   Template.editable.helpers({
     editing: function() {
       return Template.instance().isEditing.get();
     },
+    linkified: function() {
+      var context = Template.instance();
+      if (context.data.text) {
+        return linkify(context.data.text).replace(/\n/g,"<br>");
+      } else {
+        return;
+      }
+    },
+  });
+
+  Template.registerHelper("prettifyDate", function(timestamp) {
+    var d = timestamp;
+    return d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate();
   });
 }
 
